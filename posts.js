127b45dc238c04bb4b26db168fae1097b56a982c
Simone-Gallucci <149935274+Simone-Gallucci@users.noreply.github.com>
2025-06-09 11:27:58 +0200

    Update posts.js

--- posts.js
+++ posts.js
@@ -3,7 +3,7 @@ const POSTS = [
     titolo: "Benvenuto sul mio blog",
     dataPubblicazione: "05/06/2025",
     testo: "Questo è il primo post.",
-    foto: "https://www.google.com/imgres?q=immagine&imgurl=https%3A%2F%2Fmedia.istockphoto.com%2Fid%2F1057506940%2Fit%2Ffoto%2Fesplosione-di-polvere-colorata-su-sfondo-nero.jpg%3Fs%3D612x612%26w%3D0%26k%3D20%26c%3DQiQPNuNb4DnMkYLC7XHjQRVIr8F5XFA4DZ3QzMGmneo%3D&imgrefurl=https%3A%2F%2Fwww.istockphoto.com%2Fit%2Fimmagine%2Fimmagine-a-colori&docid=PCTT_vZbOvIicM&tbnid=3AvEtFxkqSXs_M&vet=12ahUKEwj58fD29eONAxXj87sIHR3sFw0QM3oECH8QAA..i&w=612&h=555&hcb=2&ved=2ahUKEwj58fD29eONAxXj87sIHR3sFw0QM3oECH8QAA"
+    foto: "https://blog.ollo.it/wp-content/uploads/elementor/thumbs/Consigli-su-come-fare-foto-allo-spazio-qn85thdixup7eg60zpvmmsiqoe3gtbq4m60dospmi8.jpg"
   },
   {
     titolo: "Secondo post senza immagine",
