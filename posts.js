cc0599654d71179d236f1916d80db18d416f3a2d
Simone-Gallucci <149935274+Simone-Gallucci@users.noreply.github.com>
2025-06-09 10:29:29 +0200

    posts.js

--- posts.js
+++ posts.js
@@ -2,8 +2,8 @@ const POSTS = [
   {
     titolo: "Benvenuto sul mio blog",
     dataPubblicazione: "05/06/2025",
-    testo: "Questo è il primo post. Sto imparando Vue.js ed è fantastico!",
-    foto: "https://via.placeholder.com/600x300"
+    testo: "Questo è il primo post.",
+    foto: "https://www.google.com/imgres?q=immagine&imgurl=https%3A%2F%2Fmedia.istockphoto.com%2Fid%2F1057506940%2Fit%2Ffoto%2Fesplosione-di-polvere-colorata-su-sfondo-nero.jpg%3Fs%3D612x612%26w%3D0%26k%3D20%26c%3DQiQPNuNb4DnMkYLC7XHjQRVIr8F5XFA4DZ3QzMGmneo%3D&imgrefurl=https%3A%2F%2Fwww.istockphoto.com%2Fit%2Fimmagine%2Fimmagine-a-colori&docid=PCTT_vZbOvIicM&tbnid=3AvEtFxkqSXs_M&vet=12ahUKEwj58fD29eONAxXj87sIHR3sFw0QM3oECH8QAA..i&w=612&h=555&hcb=2&ved=2ahUKEwj58fD29eONAxXj87sIHR3sFw0QM3oECH8QAA"
   },
   {
     titolo: "Secondo post senza immagine",
@@ -11,9 +11,9 @@ const POSTS = [
     testo: "Questo post non ha un'immagine, ma è comunque interessante."
   },
   {
-    titolo: "Vue + JSON = ❤️",
-    dataPubblicazione: "04/06/2025",
-    testo: "Oggi ho scoperto come simulare AJAX senza fetch. Geniale!",
-    foto: "https://via.placeholder.com/500x250"
+    titolo: "",
+    dataPubblicazione: "",
+    testo: "",
+    foto: ""
   }
 ];
