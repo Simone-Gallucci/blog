1e658f169780ce53ce5a3d3f9e698968dfe26817
Simone-Gallucci <149935274+Simone-Gallucci@users.noreply.github.com>
2025-06-09 10:27:05 +0200

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,19 @@
+const POSTS = [
+  {
+    titolo: "Benvenuto sul mio blog",
+    dataPubblicazione: "05/06/2025",
+    testo: "Questo è il primo post. Sto imparando Vue.js ed è fantastico!",
+    foto: "https://via.placeholder.com/600x300"
+  },
+  {
+    titolo: "Secondo post senza immagine",
+    dataPubblicazione: "03/06/2025",
+    testo: "Questo post non ha un'immagine, ma è comunque interessante."
+  },
+  {
+    titolo: "Vue + JSON = ❤️",
+    dataPubblicazione: "04/06/2025",
+    testo: "Oggi ho scoperto come simulare AJAX senza fetch. Geniale!",
+    foto: "https://via.placeholder.com/500x250"
+  }
+];
